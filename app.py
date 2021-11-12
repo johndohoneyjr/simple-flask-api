9a29815e4c7a79e7b5ea4bf006d48df1e22a5238
John Dohoney <42818490+johndohoneyjr@users.noreply.github.com>
2021-11-11 17:31:39 -0800

    Update app.py
    Runtime seems to want app and not api

--- app.py
+++ app.py
@@ -3,22 +3,22 @@ from flask import Flask, json
 results= { "documents": [{"id": 1, "score": 123456789}], "errors":[] }
 health_good = {"status": "healthy"}
 
-api = Flask(__name__)
+app = Flask(__name__)
 
-@api.route('/results', methods=['GET'])
+@app.route('/results', methods=['GET'])
 def get_results():
-  response = api.response_class(response=json.dumps(results),
+  response = app.response_class(response=json.dumps(results),
                                   status=200,
                                   mimetype='application/json')
   return response
 
-@api.route('/health', methods=['GET'])
+@app.route('/health', methods=['GET'])
 def get_health():
-  response = api.response_class(response=json.dumps(health_good),
+  response = app.response_class(response=json.dumps(health_good),
                                   status=200,
                                   mimetype='application/json')
   return response
 
 
 if __name__ == '__main__':
-    api.run() 
+    app.run(debug=True, use_reloader=True) 
